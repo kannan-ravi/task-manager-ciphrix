8f0b5cd123b31d628e3976a4c5a14868ca510632
kannan-ravi <kannanravindran00kr@gmail.com>
2025-11-14 19:18:14 +0530

    Implemented Header with ThemeSwitcher

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { RouterProvider } from "react-router";
 import { router } from "./router";
 import AppTheme from "./theme/AppTheme";
 import { CssBaseline } from "@mui/material";
+import Header from "./components/common/Header";
 
 function App(props) {
   return (
     <AppTheme {...props}>
       <CssBaseline enableColorScheme />
+      <Header />
       <RouterProvider router={router} />
     </AppTheme>
   );
---
+++ frontend/src/components/common/Header.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  alpha,
+  AppBar,
+  Container,
+  Stack,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import ThemeSwitcher from "../ui/ThemeSwitcher";
+
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  flexShrink: 0,
+  borderRadius: theme.shape.borderRadius,
+  backdropFilter: "blur(24px)",
+  border: "1px solid",
+  borderColor: (theme.vars || theme).palette.divider,
+  backgroundColor: theme.vars
+    ? `rgba(${theme.vars.palette.background.defaultChannel} / 0.4)`
+    : alpha(theme.palette.background.default, 0.4),
+  boxShadow: (theme.vars || theme).shadows[1],
+  padding: "8px 16px",
+}));
+
+const Header = () => {
+  return (
+    <AppBar
+      position="sticky"
+      enableColorOnDark
+      sx={{
+        boxShadow: 0,
+        bgcolor: "transparent",
+        backgroundImage: "none",
+        mt: "calc(var(--template-frame-height, 0px) + 28px)",
+      }}
+    >
+      <Container maxWidth="lg">
+        <StyledToolbar variant="dense" disableGutters>
+          <Typography sx={{ color: "text.primary" }}>Task Manager</Typography>
+          <Stack
+            direction="row"
+            alignItems="center"
+            spacing={1}
+            sx={{ marginLeft: "auto" }}
+          >
+            <Stack direction="row" alignItems="center">
+              <ThemeSwitcher />
+            </Stack>
+          </Stack>
+        </StyledToolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;
