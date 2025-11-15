dd2686c4777106229f16a7206844b0353a99e236
kannan-ravi <kannanravindran00kr@gmail.com>
2025-11-15 21:56:41 +0530

    feat: added add and edit task pages to the router

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import TaskCreate from "./pages/task/TaskCreate";
+import TaskEdit from "./pages/task/TaskEdit";
 
 export const router = createBrowserRouter([
   {
@@ -15,5 +17,13 @@ export const router = createBrowserRouter([
   {
     path: "/register",
     element: <Register />,
+  },
+  {
+    path: "/task/create",
+    element: <TaskCreate />,
+  },
+  {
+    path: "/task/edit/:id",
+    element: <TaskEdit />,
   },
 ]);
