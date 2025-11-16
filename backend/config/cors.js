57454328bdd8533db5f19fbed72656b6f6b3078b
kannan-ravi <kannanravindran00kr@gmail.com>
2025-11-16 23:23:43 +0530

    fix: Hard coded the frontend domain in cors

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -3,7 +3,7 @@ import { configDotenv } from "dotenv";
 configDotenv();
 
 export const corsOptions = {
-  origin: [process.env.CLIENT_URL],
+  origin: [process.env.CLIENT_URL, "https://task-manager-ciphrix.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
